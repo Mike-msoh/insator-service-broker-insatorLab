a2fceb331d5769a219c6cf43ff1bcd3ac0999d45
msoh <ohms@kr.ibm.com>
2017-03-10 10:42:24 +0900

    add service url

--- service.py
+++ service.py
@@ -12,6 +12,7 @@ def insatorsvc():
 	    'bindable' : True, 
 	    'tags' : ['private'], 
 	    'plan_updateable' : False,
+	    'serviceKeysSupported' : True,
 	    'plans' : [insator_plans.plan_a(), insator_plans.plan_b()],
 	    'dashboard_client' : {
 	        'id' : uuid.uuid4(),
@@ -33,7 +34,14 @@ def insatorsvc():
 	                'title' : 'Connectivity',
 	                'description' :' Insator Service is built to let you connect easily and to all of your services and applications. You can start analyzing your data immediately with familiar tools.'
 	            }
-	        ]
+	        ],
+	        'featuredImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_64x64.png',
+	        'imageUrl' : 'http://fido-ui-service.mybluemix.net/images/images/nexsign/Nexsign_icon_50x50.png',
+	        'mediumImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_32x32.png',
+	        'smallImageUrl' : 'http://fido-ui-service.mybluemix.net/images/nexsign/Nexsign_icon_24x24.png',
+	        'documentationUrl' : 'http://www.samsungsds.com/us/en/solutions/off/nex/nexsign.html',
+	        'instructionsUrl' : 'http://www.samsungsds.com/us/en/solutions/off/nex/nexsign.html',
+	        'termsUrl' : 'https://media.termsfeed.com/pdf/terms-and-conditions-template.pdf'
 	    }
 	}
 	return insator_service
