a25794f3798255bbf4b72b0358c2ae527db0840a
msoh <ohms@kr.ibm.com>
2017-03-10 13:13:45 +0900

    lange updated

--- insator-service-broker.py
+++ insator-service-broker.py
@@ -289,7 +289,7 @@ def catch_all(path):
     page += '<h2>This is a sample service broker for Samsung SDS : insator Solution</h2>'
     page += '<p>See for details.</p>'
     page += '<p>You requested path: /%s </p>' % path
-    page += '<p> Browser Language : %s </p>' % request.accept_languages
+    page += '<p> Browser Language : %s </p>' % request.accept_languages.best_match(['de', 'fr', 'en'])
     return page
 
 
